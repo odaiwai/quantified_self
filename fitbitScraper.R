2ab51b92c0d3ec4e1fa924165600acc52f5110ea
odaiwai <odaiwai@diaspoir.net>
2016-03-16 21:16:59 +0800

    tried and failed to use WWW-Mechanise to automatically retrieve fitbit monthly daya, but it was easier to just do it manually.

---
+++ fitbitScraper.R
@@ -0,0 +1,23 @@
+#install.packages("fitbitScraper")
+library("fitbitScraper")
+
+cookie <- login(email="odaiwai@gmail.com", password="ceG4Ayb1nAf5hEk")
+str (cookie)
+# 15_min_data "what" options: "steps", "distance", "floors", "active-minutes", "calories-burned"
+df <- get_intraday_data(cookie, what="steps", date="2015-01-21")
+library("ggplot2")
+ggplot(df) + geom_bar(aes(x=time, y=data, fill=data), stat="identity") +
+             xlab("") +ylab("steps") +
+             theme(axis.ticks.x=element_blank(),
+                   panel.grid.major.x = element_blank(),
+                   panel.grid.minor.x = element_blank(),
+                   panel.grid.minor.y = element_blank(),
+                   panel.background=element_blank(),
+                   panel.grid.major.y=element_line(colour="gray", size=.1),
+                   legend.position="none")
+
+# daily_data "what" options: "steps", "distance", "floors", "minutesVery", "caloriesBurnedVsIntake"
+df <- get_daily_data(cookie, what="steps", start_date="2015-01-13", end_date="2015-01-20")
+ggplot(df) + geom_point(aes(x=time, y=data))
+
+
