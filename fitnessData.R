ad87675e433d60cebb0e66902f52799e17748c00
odaiwai <odaiwai@diaspoir.net>
2016-05-01 11:12:57 +0800

    Added a new text file discussing my vision problems, and committed the latest versions of the various data retrieval and parsing scripts.

--- fitnessData.R
+++
@@ -1,9 +0,0 @@
-# R Script to input the various fitness Data files
-# and come to a conclusion
-#
-
-#mfpData <- read.csv("myFitnessPalData.dat", sep="\t", header = TRUE)
-mfpData <- read.csv("weightData.dat", sep="\t", header = TRUE)
-
-# Need to consider a Linear model of the form:
-# today's Weight = Starting Weight + previous day's calories / (cal/kg) + ... (previous calories too)
