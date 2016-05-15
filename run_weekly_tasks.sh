349cd6b3f32aab33c7dbc5fb1568db0663e27f9a
odaiwai <odaiwai@diaspoir.net>
2016-05-15 16:39:03 +0800

    Removed Limit command from weekly tasks.
    This shows the right thing.

--- run_weekly_tasks.sh
+++ run_weekly_tasks.sh
@@ -4,5 +4,5 @@
 ./getMyFitnessPalData.pl
 ./parse_myfitnesspaldata.pl
 ./parse_fitbit_data.pl
-sqlite3 myfitnesspal.sqlite -csv 'select * from daily_summary join calories_burned using (date) limit 21;'
-sqlite3 fitbit_data.sqlite -csv 'select date, calories_burned from fitbit_data limit 21;'
+sqlite3 myfitnesspal.sqlite -csv 'select * from daily_summary join calories_burned using (date);'
+sqlite3 fitbit_data.sqlite -csv 'select date, calories_burned from fitbit_data limit;'
