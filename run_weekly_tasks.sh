4ee4058ade97ecfad9145f41b6ae6a59c1217465
Dave O'Brien (macbookpro) <github@diaspoir.net>
2016-09-15 08:55:10 +0700

    Modified Weekly Run task to include fitbit data for regression.

--- run_weekly_tasks.sh
+++ run_weekly_tasks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sqlite='/usr/bin/sqlite3'
 # script to run the weekly tasks
 ./getMyFitnessPalData.pl
 ./parse_myfitnesspaldata.pl
 ./parse_fitbit_data.pl
-sqlite3 myfitnesspal.sqlite -csv 'select * from daily_summary join calories_burned using (date);'
-#sqlite3 fitbit_data.sqlite -csv 'select date, calories_burned from fitbit_data;'
+$sqlite myfitnesspal.sqlite -csv 'select * from daily_summary join calories_burned using (date);'
+#$sqlite fitbit_data.sqlite -csv 'select date, calories_burned from fitbit_data;'
+/usr/bin/sqlite3 fitbit_data -csv 'select date, Calories_burned, Total_steps, Traveled, Floors_climbed, Sedentary, Lightly_active, Fairly_active, Very_active from [fitbit_data];'
