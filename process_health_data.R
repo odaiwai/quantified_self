12f0aa7cf38b9af2dd2552463333caf86078f4ee
Dave O'Brien (XCode) <github@diaspoir.net>
2019-02-17 16:10:18 +0800

    Modified Rstudio Document to work with databases

--- process_health_data.R
+++ process_health_data.R
@@ -3,33 +3,28 @@
 library(devtools)
 library(ggplot2)
 #library(sqlutils)
-#library(RSQLite)
-#library(curl)
+library(DBI) # ref: https://db.rstudio.com/databases/sqlite/
 
-# basic dataframes
-weightData <- read.csv("weight_bp_data.csv", sep=";", header=TRUE)
-nutritionData <- read.csv("myFitnessPalData.dat", sep=";")
-fitbitData <- read.csv("fitbit_data.csv", sep=";")
-
+# connnect to the database and get the data
+con <- dbConnect(RSQLite::SQLite(), "~/Documents/health/analyse_health_data/health_data.sqlite")
+#weightData <- dbReadTable(con, "ss_physical")
+weightData <- dbGetQuery(con, "select * from ss_physical where timestamp > \'2008™%\'")
 # fix the dates
-weightData$newDate <- as.POSIXct(weightData$Date, format='%d-%B-%Y %H:%M')
-nutritionData$newDate <- as.Date(nutritionData$Date, format='%d-%b-%y')
-fitbitData$newDate <- as.Date(fitbitData$Date, format=' %B %d, %Y')
-
-# some calculations
-nutritionData$CalorieDeficit <- (nutritionData$Calories - nutritionData$FB.Cals)
-weightData$BMI <- (weightData$Weight/(weightData$Height^2))
+weightData$date <- as.POSIXct(weightData$timestamp, tz="", format='%Y-%m-%d %H:%M:%S')
+#nutritionData$newDate <- as.Date(nutritionData$Date, format='%d-%b-%y')
+#fitbitData$newDate <- as.Date(fitbitData$Date, format=' %B %d, %Y')
 
 ## What's the class?
 #str(weightData)
-#str(nutritionData)
-#str(fitbitData)
 
 # make a plot object
-weightPlot <- ggplot(weightData, aes(x = newDate,y = Weight, size=BMI, color=BMI))
-weightPlot + geom_point() 
-weightPlot + geom_smooth(method="loess") 
-weightPlot + labs(x="date", y="Weight (kg)")
+weightPlot <- ggplot(weightData, aes(x = date,y = weight, color=bmi)) +
+  geom_point() +  
+  geom_smooth(method = "loess") +
+  labs(x="date", y="Weight (kg)") +
+  ggtitle("Body Weight Over Time")
+
+weightPlot
 
 # Need to make a model of intake over days n-x to n-1 and compare it to weight at day n
 
@@ -37,8 +32,8 @@ weightPlot + labs(x="date", y="Weight (kg)")
 ## Take our data and make a scatter plot
 
 #p <- ggplot(weightData, aes(x=Age, y=Bodyfat_kg)) + geom_point() + geom_smooth(method="loess")                                                                      )
-fitbitPlot <- ggplot(fitbitData, aes(x=newDate, y=Total_steps, size=Very_active)) + geom_point()
-nutriPlot <- ggplot(nutritionData, aes(x=newDate, y=Calories))+geom_point() + geom_smooth(method="loess") 
+#fitbitPlot <- ggplot(fitbitData, aes(x=newDate, y=Total_steps, size=Very_active)) + geom_point()
+#nutriPlot <- ggplot(nutritionData, aes(x=newDate, y=Calories))+geom_point() + geom_smooth(method="loess") 
 
 # Things to do:
 # Make a dataframe with Date, Weight for that Date, Calories in for last x days, calories out for last x days
