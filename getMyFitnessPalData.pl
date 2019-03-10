5fb52f6a5ce482b550298a8a21ee5463e1e96c71
Dave O'Brien (XCode) <github@diaspoir.net>
2019-03-10 14:39:25 +0800

    fixed downloading for mfp data.

--- getMyFitnessPalData.pl
+++ getMyFitnessPalData.pl
@@ -17,6 +17,7 @@ my $loginurl = "$siteurl/account/login";
 my $reporturl = "$siteurl/reports/printable_diary/$username";
 #\?from=$startdate\&to=$enddate";
 my $verbose = 1;
+my $getall = 0;
 
 my $agent =  WWW::Mechanize->new( autocheck => 1);
 #$agent->get($loginurl);
@@ -39,14 +40,61 @@ if ($agent->success) {
     $agent->submit(input => $submits[0]);
 
 }
-my $start_year = 2018;
-my $end_year = `date +%Y`;
-for my $year ($start_year..$end_year) {
-    print "Year: $year\n";
-    my $startdate = "$year-01-01";
-    my $enddate = "$year-12-31";
-    my $outfile = "../health_data/myFitnessPal_data/mfp_report_$year";
+
+# Get the  current time - faster than running `date`
+my ($sec,$min,$hour,$mday,$tmon,$tyear,$wday,$yday,$isdst) =  localtime(time);
+if ( $getall) {
+    my $start_year = 2014 ; # normally just do the current month
+    my $end_year = $tyear + 1900;
+    # reporting on the whole year doesn't work anymore
+    # go to monthly reporting
+    for my $year ($start_year..$end_year) {
+        for my $month (qw/01 02 03 04 05 06 07 08 09 10 11 12/) {
+            print "Month: $year-$month\n";
+            my $startdate = "$year-$month-01";
+            my $lastday = last_day_of_month($year, $month);
+            my $enddate = "$year-$month-$lastday";
+            my $outfile = "../health_data/myFitnessPal_data/mfp_report_$year$month";
+            get_printable_report($agent, $reporturl, $startdate, $enddate, $outfile);
+        }
+    }
+} else {
+    my $this_year = $tyear + 1900;
+    my $this_month = sprintf("%02d", $tmon + 1);
+    
+    print "Month: $this_year-$this_month\n";
+    my $startdate = "$this_year-$this_month-01";
+    my $lastday = last_day_of_month($this_year, $this_month);
+    my $enddate = "$this_year-$this_month-$lastday";
+    my $outfile = "../health_data/myFitnessPal_data/mfp_report_$this_year$this_month";
     get_printable_report($agent, $reporturl, $startdate, $enddate, $outfile);
+}
+
+sub last_day_of_month {
+    # Return the last day of the month
+    my $year  = shift;
+    my $month = shift;
+    my @months   = qw/01 02 03 04 05 06 07 08 09 10 11 12/;
+    my @lastdays = qw/31 28 31 30 31 30 31 31 30 31 30 31/;
+    if ( is_leap_year($year) ) {
+        $lastdays[1] = 29;
+    }
+    my %lastdayofmonth;
+    @lastdayofmonth{@months} = @lastdays;
+    my $lastday = $lastdayofmonth{$month};
+    return $lastday;
+}
+
+sub is_leap_year {
+    # return 1 for leap year, 0 otherwise
+    my $year = shift;
+    
+    my $leapyear = 0;
+    if ( $year %   4 == 0) {$leapyear = 1;}
+    if ( $year % 100 == 0) {$leapyear = 0;}
+    if ( $year % 400 == 0) {$leapyear = 1;}
+    
+    return $leapyear;
 }
 
 sub get_printable_report {# Go get the report URL
